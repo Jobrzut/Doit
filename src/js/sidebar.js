82a2fe179359b0f93eb3bc32d7d4d951d1ec422d
Julek <julek@geminidesign.pl>
2025-08-18 21:58:18 +0200

    Fix sidebar selector

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -3,7 +3,7 @@ const sidebar = document.querySelector(".sidebar");
 import { displayProject } from "./displayproject";
 
 sidebar.addEventListener("click", (event) => {
-    if (event.target.tagName.toLowerCase() === "li" && !event.target.classList.contains("add_task")) {
+    if (event.target.tagName.toLowerCase() === "li" && event.target.parentElement.classList.contains("userAdded")) {
         let top_list = document.querySelectorAll(".sidebar ul li:not(.add_task)");
         top_list.forEach(li => {
            li.className = "";
